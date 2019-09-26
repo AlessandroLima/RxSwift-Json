13ddae8e2ea8fa770f8a64bafe154e8e0df7fe7f
aletlima <aletlima@gmail.com>
2019-09-26 15:40:48 -0300

    Inicial

---
+++ TesteJson/Entity/GitHubEntity.swift
@@ -0,0 +1,31 @@
+//
+//  Repository.swift
+//  TesteJson
+//
+//  Created by Alessandro on 06/09/19.
+//  Copyright © 2019 Alessandro. All rights reserved.
+//
+
+import Foundation
+
+struct Owner:Codable{
+    let login:String
+    let avatar_url:String
+}
+
+struct Item:Codable{
+    let id: Int
+    let name: String
+    let language: String
+    let owner:Owner
+    let stargazers_count:Int
+}
+
+struct GitHubEntity:Codable{
+    
+    let total_count:Int
+    let incomplete_results:Bool
+    let items:[Item]
+}
+
+
